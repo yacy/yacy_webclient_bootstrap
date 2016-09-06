b4fb9808e7b3e4e6ddbf3b4de593f624c92ca535
Michael Peter Christen <mc@yacy.net>
2016-09-06 17:07:46 +0200

    fix for image display

--- js/yacysearch.js
+++ js/yacysearch.js
@@ -11,10 +11,10 @@ var SearchModel = Backbone.Model.extend({
   defaults:{hl:'false',query:'',start:'0',rows:'100',layout:'paragraph',startTime:new Date(),servlet:"index.html",contentdom:"text"},
 
   url:function(){
-    return searchURL + '&hl=' + this.attributes.hl + '&query=' + this.attributes.query + '&startRecord=' + this.attributes.start + '&maximumRecords=' + this.attributes.rows + '&contentdom=' + (this.attributes.layout == "images" ? "image" : this.attributes.contentdom);
-    //u = searchURL + '&hl=false&wt=yjson&facet=true&facet.mincount=1&facet.field=host_s&facet.field=url_file_ext_s&facet.field=url_protocol_s&facet.field=author_sxt&facet.field=collection_sxt&start=' + this.attributes.start + '&rows=' + this.attributes.rows + '&query=' + this.attributes.query;
-    //if (this.attributes.layout == "images") u += "&fq=url_file_ext_s:(png+OR+jpeg+OR+jpg+OR+gif)";
-    //return u;
+    //return searchURL + '&hl=' + this.attributes.hl + '&query=' + this.attributes.query + '&startRecord=' + this.attributes.start + '&maximumRecords=' + this.attributes.rows + '&contentdom=' + (this.attributes.layout == "images" ? "image" : this.attributes.contentdom);
+    u = searchURL + '&hl=false&wt=yjson&facet=true&facet.mincount=1&facet.field=host_s&facet.field=url_file_ext_s&facet.field=url_protocol_s&facet.field=author_sxt&facet.field=collection_sxt&start=' + this.attributes.start + '&rows=' + this.attributes.rows + '&query=' + this.attributes.query;
+    if (this.attributes.layout == "images") u += "&fq=url_file_ext_s:(png+OR+jpeg+OR+jpg+OR+gif)";
+    return encodeURI(u);
   },
 
   parse:function(resp){
@@ -89,7 +89,7 @@ var RowModel = Backbone.Model.extend({
     var link = this.attributes.link;
     if (link == null || link == "") link = this.attributes.image;
     if (link == null) return "";
-    var image = this.attributes.image;
+    var image = this.attributes.image ? this.attributes.image : link;
     var protocol = "";
     var host = "";
     // extract the path
